c22e48163221cf0eea4cc3e5ba9c0a5ebc2dd613
prathap <jvprathap.2024@gmail.com>
2025-03-04 12:27:55 +0530

    okk in branch1

--- BasicCalculator.java
+++ BasicCalculator.java
@@ -10,7 +10,7 @@ public class BasicCalculator {
 	public static void main(String[] args)
 	{
           String str1 ="hi";
-		  String str="bye";
+		  String str="hi";
 		  System.out.println(str1.hashCode);
 		  System.out.println(str2.hashCode);
 
