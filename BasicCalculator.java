5bfae6da5929cb6d9165f0c296844d77e536a2a5
prathap <jvprathap.2024@gmail.com>
2025-03-04 12:33:12 +0530

    brcnch okk all  by p

--- BasicCalculator.java
+++ BasicCalculator.java
@@ -10,7 +10,7 @@ public class BasicCalculator {
 	public static void main(String[] args)
 	{
           String str1 ="hi";
-		  String str="hi";
+		  String str="bye";
 		  System.out.println(str1.hashCode);
 		  System.out.println(str2.hashCode);
 
