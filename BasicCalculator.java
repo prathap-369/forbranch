df5d9fe5777529a473b19747b2aac4386040ae95
prathap <jvprathap.2024@gmail.com>
2025-03-04 13:18:28 +0530

    now all are ok brach2

--- BasicCalculator.java
+++ BasicCalculator.java
@@ -12,7 +12,7 @@ public class BasicCalculator {
           String str1 ="hi";
 		  String str="hi";
 		  System.out.println(str1.hashCode);
-		  System.out.println(str2.hashCode);
+		  System.out.println(str.hashCode);
 
 		// Stores two numbers
 		double num1, num2;
